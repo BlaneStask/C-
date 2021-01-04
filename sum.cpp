cabe3f3509040f7e7e9d0eb7013adceeeb05a5dd
Blane Staskiewicz <bas6248@psu.edu>
2021-01-04 12:11:34 -0500

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -3,26 +3,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     int value = 0;
     int sum = 0;
-    
     cout << "Enter a positive integer value." ;
     cin >> value;
-    
     //For positive integers
-    for (int i = 1; i <= value; i++)
-        {
-            sum += i;
-            
-            cout << "Sum of the integers 1 - " << value << " is " << sum << endl;
-        }
+    for (int i = 1; i <= value; i++){
+        sum += i;
+        cout << "Sum of the integers 1 - " << value << " is " << sum << endl;
+    }
     //For negative integers
-    while (value < 0)
-        {
-            cout << value << "is a negative integer.";
-        }
-    
+    while (value < 0){
+        cout << value << "is a negative integer.";
+    }
     return 0;
 }
