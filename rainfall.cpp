0a0baa880add74437c750ad0bc59913ef9b62980
Blane Staskiewicz <bas6248@psu.edu>
2021-01-04 12:08:41 -0500

    Update rainfall.cpp

--- rainfall.cpp
+++ rainfall.cpp
@@ -2,39 +2,29 @@
 
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
-int main()
-{
+int main(){
     int years = 0;
     const int month = 12;
     float totalr = 0.0f;
     float rain = 0.0f;
-    
     cout << "Enter number of years: " << endl;
     cin >> years;
-    
     //Needs to be at least a year
-    while (years < 1)
-    {
+    while (years < 1){
         cout << "Must be at least 1 year";
         cin >> years;
     }
-    
     //Years looped
-    for (int i = 1; i <= years; i++)
-    {
+    for (int i = 1; i <= years; i++){
         //Months looped
-        for (int months = 1; months <= month; months++)
-        {
+        for (int months = 1; months <= month; months++){
             //Recorded rainfall for the month
             cout << "Enter the rainfall for " << months << " month: ";
             cin >> rain;
-            
             //Negative rainfall
-            while (rain < 0)
-            {
+            while (rain < 0){
                 cout << "This cannot be a negative value ";
                 cin >> rain;
             }
@@ -42,10 +32,8 @@ int main()
             totalr += rain;
         }
     }
-    
     cout << "Number of months: " << years * 12 <<endl;
     cout << "Total rainfall: " << setprecision(2) << fixed << totalr << " inches" << endl;
     cout << "Average rainfall: " << setprecision(2) << fixed << totalr / (years * 12) << " inches";
-    
     return 0;
 }
